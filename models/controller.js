b182ca6f4af118379bfb27907999f913c448e394
troy <gordontaaj@gmail.com>
2019-01-08 12:04:12 -0800

    finishing

---
+++ models/controller.js
@@ -0,0 +1,9 @@
+// Dependencies
+var express = require("express");
+
+var router = express.Router();
+
+router.get("/", function (req, res) {
+        res.render("main");
+    });
+module.exports = router;
